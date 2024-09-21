7e9b1c5ccdb45bceafb3ec075d0ed6e22603da80
Srinu Chawan <srinusabhavath24@gmail.com>
2024-09-21 20:11:36 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -1,28 +1,38 @@
+
+
 import pyttsx3
 import speech_recognition as sr
 import datetime
 import os
+import pywhatkit
 import webbrowser
+
+from speech_recognition import Microphone
+
 engine = pyttsx3.init()
 voices = engine.getProperty("voices")
-engine.setProperty("voices",voices[0].id)
+print(voices[0].id)
+engine.setProperty("voices",voices[1].id)
 def say(audio):
     engine.say(audio)
     engine.runAndWait()
+
     return audio
 def takeCommand():
     r = sr.Recognizer()
+    source: Microphone
     with sr.Microphone() as source:
 
-        r.pause_threshold = 0.5
+        r.pause_threshold =0.5
+
         audio = r.listen(source)
-        # print("Recognizing...")
+       # print("Recognizing...")
         try:
             query = r.recognize_google(audio,language='en-in')
             print(f"User said: {query}\n")
             return query
         except Exception as e:
-            return "some error occured .srry from jarvis"
+            return "some error occured sorry from jarvis"
 
 
 def wish():
@@ -36,7 +46,7 @@ def wish():
     say("I am Jarvis tell me how can I help you!")
 
 if __name__ == '__main__':
-    say("Hello srinu!")
+    say("Hello boss!")
     wish()
     while True:
         print("Say something")
@@ -45,9 +55,39 @@ if __name__ == '__main__':
 
 
         query = takeCommand()
-        sites = [['YouTube','https://www.youtube.com'],['Google','https://www.google.com'],['Facebook','https://www.facebook.com'],['instagram','https://www.instagram.com']]
+        sites = [['YouTube','https://www.youtube.com'],['Google','https://www.google.com'],['Facebook','https://www.facebook.com'],['instagram','https://www.instagram.com'],['my portal','http://tkrec.in//student//']]
         for site in sites:
 
             if f"open {site[0]}".lower() in query.lower():
                 say(f"opening {site[0]} sir....")
                 webbrowser.open(site[1])
+        if 1:
+            if "hi jarvis" in query.lower():
+                say(f"hi krishna sir....")
+            elif "jarvis" in query.lower():
+                say(f"hi srinu  sir....")
+
+
+            elif "hello jarvis" in query.lower():
+                say(f"hello aaneel sir....")
+            elif "how are you" in query.lower():
+                say(f"I am fine. How are you?")
+            elif "email password" in query.lower():
+                say(f"9908584745 sir....")
+            elif "open music" in query.lower():
+                musicPath = "C:\\Users\Srinivas\\Music\\MEmu Music\\[iSongs.info] 01 - Chirunama Thana Chirunama.mp3"
+                os.startfile(musicPath)
+            elif "open photo" in query.lower():
+                photoPath ="C:\\Users\Srinivas\\Pictures\\Screenshots\\Screenshot 2024-05-15 130434.png"
+                os.startfile(photoPath)
+            elif ("good night") in query.lower():
+                say(f"good night madam....")
+
+            else:
+                say(f"say it again")
+
+
+
+
+
+
